5010672c8e968885ea5797fa8c237c29e5aed9c4
avivtz11 <avivtz1@gmail.com>
2021-08-05 19:26:50 -0400

    now get next word gets **char so that it's easier to iterate with it - started to implement usage

--- first_pass.c
+++ first_pass.c
@@ -4,7 +4,7 @@
 
 #define MAX_LINE 82
 
-char *get_next_word(char *line);
+char *get_next_word(char **line);
 
 
 void first_pass(FILE *assembly_fp)
@@ -12,6 +12,7 @@ void first_pass(FILE *assembly_fp)
 	char line[MAX_LINE];
 	int line_num;
 	char *current_word;
+	char *line_ptr;
 	int does_line_define_symbol;
 	int ic = 100;
 	int dc = 0;
@@ -19,46 +20,52 @@ void first_pass(FILE *assembly_fp)
 	line_num = 1;
 	while(fgets(line, sizeof(line), assembly_fp)) /*TODO maybe error if more than MAX_LINE?*/
 	{
+		line_ptr = line;
 		does_line_define_symbol = 0;
-		current_word = get_next_word(line);
-		if((*current_word != '\n') && (*current_word != ';')) /*not comment or empty*/
+		current_word = get_next_word(&line_ptr);
+		line_ptr += strlen(current_word);
+		while(line + strlen(line) >= line_ptr)
 		{
-			if(*(current_word + strlen(current_word) - 1) == ':') /*has symbol*/
+			if((*current_word != '\n') && (*current_word != ';')) /*not comment or empty*/
 			{
-				does_line_define_symbol = 1;
-			}
+				if(*(current_word + strlen(current_word) - 1) == ':') /*has symbol*/
+				{
+					does_line_define_symbol = 1;
+					current_word = get_next_word(line_ptr);
+					line_ptr += strlen(current_word);
+				}
 
-			if((strcmp(current_word, ".dh") == 0) || (strcmp(current_word, ".dw") == 0) || (strcmp(current_word, ".db") == 0) || (strcmp(current_word, ".asciz") == 0)) /*is data storage line*/
-			{
-				/*TODO insert to symbol table, and put data in data section 7-8*/
-			}
+				if((strcmp(current_word, ".dh") == 0) || (strcmp(current_word, ".dw") == 0) || (strcmp(current_word, ".db") == 0) || (strcmp(current_word, ".asciz") == 0)) /*is data storage line*/
+				{
+					/*TODO insert to symbol table, and put data in data section 7-8*/
+				}
 
 			
+			}
+			free(current_word); /*TODO maybe change this to array*/
 		}
-		free(current_word); /*TODO maybe change this to array*/
 	}
 }
 
 
-char *get_next_word(char *line)
+char *get_next_word(char **current_char)
 {
 	char *result_word;
 	int result_word_length;
 	char *first_word_char;
-	char *current_char = line;
 
-	while((*current_char == ' ') || (*current_char == '\t')) /*skipping prefix whitespace*/
+	while((**current_char == ' ') || (**current_char == '\t')) /*skipping prefix whitespace*/
 	{
-		current_char++;
+		(*current_char)++;
 	}
-	first_word_char = current_char;
+	first_word_char = *current_char;
 
-	while((*current_char != ' ') && (*current_char != '\t') && (*current_char != '\n')) /*TODO check this*/
+	while((**current_char != ' ') && (**current_char != '\t') && (**current_char != '\n')) /*TODO check this*/
 	{
-		current_char++;
+		(*current_char)++;
 	}
 
-	result_word_length = current_char - first_word_char;
+	result_word_length = (*current_char) - first_word_char;
 	result_word = (char *) malloc(result_word_length + 1);
 	memcpy(result_word, first_word_char, result_word_length);
 	result_word[result_word_length] = '\0';
