1534a9fbabb285608c4833ef878f48e6e8223a25
avivtz11 <avivtz1@gmail.com>
2021-08-23 17:22:38 -0400

    started building second pass - stopping to check things on first

--- second_pass.c
+++ second_pass.c
@@ -2,8 +2,29 @@
 
 #include "symbol_table.h"
 
+#define MAX_LINE 82
+
 
 void second_pass(FILE *assembly_fp, SymbolTable *symbol_table, char *data_segment)
 {
+	char line[MAX_LINE];
+	int line_num;
+	/*char *current_word;
+	char *line_ptr;
+	int err_flag;*/
 
+	line_num = 1;
+	while(fgets(line, sizeof(line), assembly_fp)) /*TODO maybe error if more than MAX_LINE?*/
+	{
+		/*line_ptr = line;
+		current_word = get_next_word(&line_ptr);
+		line_ptr += strlen(current_word);
+		while(line + strlen(line) >= line_ptr)
+		{
+			
+		}
+		
+		free(current_word);*/
+	}
+	line_num++;
 }
