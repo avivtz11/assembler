ef1819edbcc8a649624bd3ca269fa3f3ce9258e5
avivtz11 <avivtz1@gmail.com>
2021-08-29 06:37:30 -0400

    j call

--- command_parsers.c
+++ command_parsers.c
@@ -14,6 +14,7 @@ void code_I_branching_to_binary(char **result, char *command, char **line_ptr, S
 void code_I_load_store_to_binary(char **result, char *command, char **line_ptr, SymbolTable *symbol_table, int ic, int *err_code);
 void code_J_jmp_to_binary(char **result, char *command, char **line_ptr, SymbolTable *symbol_table, int ic, int *err_code);
 void code_J_la_to_binary(char **result, char *command, char **line_ptr, SymbolTable *symbol_table, int ic, int *err_code);
+void code_J_call_to_binary(char **result, char *command, char **line_ptr, SymbolTable *symbol_table, int ic, int *err_code);
 void handle_param(char **result, char **line_ptr, SymbolTable *symbol_table, int ic, int *err_code, int line_bin_offset, int bin_size, void (*code_param)(char *, char ** ,SymbolTable * ,int ,int *));
 
 
@@ -43,13 +44,34 @@ void(*get_command_parsing_function(char *command))(char **, char *, char **, Sym
 	if(strcmp(command, "la") == 0)
 		return code_J_la_to_binary;
 
+	if(strcmp(command, "call") == 0)
+		return code_J_call_to_binary;
+
 	return NULL;
 }
 
 
-void code_J_la_to_binary(char **result, char *command, char **line_ptr, SymbolTable *symbol_table, int ic, int *err_code)
+void code_J_call_to_binary(char **result, char *command, char **line_ptr, SymbolTable *symbol_table, int ic, int *err_code)
 {
-	
+	malloc_with_error((void **)result, 4*8 + 1, "couldn't allocate memory");
+
+	code_opcode(result, command);/*opcode*/
+
+	handle_param(result, line_ptr, symbol_table, ic, err_code, 6, 26, code_register_address);/*reg + address*/
+	if(*err_code)
+		return;
+
+	if((**line_ptr != '\n') && (**line_ptr != '\0'))
+	{
+		*err_code = 4;
+		return;
+	}
+	(*result)[32] = '\0';
+}
+
+
+void code_J_la_to_binary(char **result, char *command, char **line_ptr, SymbolTable *symbol_table, int ic, int *err_code)
+{	
 	malloc_with_error((void **)result, 4*8 + 1, "couldn't allocate memory");
 
 	code_opcode(result, command);/*opcode*/
